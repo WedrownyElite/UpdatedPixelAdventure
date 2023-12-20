21bb82c5c8c580a64b0fa1328bc58817dd965d9a
WedrownyElite <chris.sliwinski@gmail.com>
2023-12-20 15:27:56 -0600

    fuck

--- PixelAdventureUpdated/EnemyFunctions.cpp
+++ PixelAdventureUpdated/EnemyFunctions.cpp
@@ -3,5 +3,8 @@
 olc::vf2d EnemyFunctions::HitKnockback(olc::PixelGameEngine* pge, int IsHit, olc::vf2d EnemyPos, olc::vf2d PlayerPos) {
 	if (IsHit == 1) {
 		olc::vf2d dir = (PlayerPos - EnemyPos).norm();
+
+		EnemyPos += dir;
 	}
+	return EnemyPos;
 }
